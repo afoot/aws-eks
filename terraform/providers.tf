9f359abd96b0fcc4eeaae298eb1fefdefbe7b381
Intourist <19631339+afoot@users.noreply.github.com>
2025-06-24 16:50:27 -0400

    Create providers.tf

---
+++ terraform/providers.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = local.tags
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.67.0"
+    }
+  }
+
+  required_version = ">= 1.4.2"
+}
